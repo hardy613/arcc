198d11fc23e26eac917de00f9249d7907c8cc249
shardy <shardy@bsl.com>
2016-04-25 10:06:21 -0400

    comments

--- arcc.ino
+++ arcc.ino
@@ -1,11 +1,11 @@
 #include <SoftwareSerial.h>
 /**
- * @file: basic set up for blueTooth connection with my phone
- *        I would like to add a WiiMote in the future
- *        The module we are connecting to is an HC-06 BlueTooth chip;
- *        right now we are just reading the data we send it, just need a
- *        steering wheel (hence wiimote).
- */
+   @file: basic set up for blueTooth connection with my phone
+          I would like to add a WiiMote in the future
+          The module we are connecting to is an HC-06 BlueTooth chip;
+          right now we are just reading the data we send it, just need a
+          steering wheel (hence wiimote).
+*/
 
 #define rxPin 4
 
@@ -23,59 +23,62 @@ String blueToothMessage;
 SoftwareSerial blueTooth(rxPin, txPin);
 
 /**
- * 
- */
+
+*/
 void setup()
 {
- Serial.begin(baudRate);
+  Serial.begin(baudRate);
 
- blueTooth.begin(baudRate);
- 
- pinMode(rxPin, INPUT);
- 
- pinMode(txPin, OUTPUT);
- 
- Serial.println("Starting: ...");
+  blueTooth.begin(baudRate);
+
+  pinMode(rxPin, INPUT);
+
+  pinMode(txPin, OUTPUT);
+
+  Serial.println("Starting: ...");
 }
- 
+
 /**
- * 
- */
+
+*/
 void loop()
 {
   Serial.flush();
 
   blueTooth.flush();
-  
-  if (!blueTooth.available()) return;
-  
-  while(blueTooth.available()) 
-  {    
+
+  if (!blueTooth.available())
+  {//end if there is nothing to read
+    return;
+  }
+
+  while (blueTooth.available())
+  {//we read the char and add it to the string
     blueToothVal = blueTooth.read();
-  
+
     blueToothMessage += blueToothVal;
-    
+
     Serial.print("Captured Char: ");
-    
+
     Serial.println(blueToothVal);
-    
+
     blueToothVal = NULL;
   }
 
-  if(blueToothMessage.length()) 
-  {
+  if (blueToothMessage.length())
+  {//output captured data to the serial montior
     Serial.print("Captured Message: ");
-    
+
     Serial.println(blueToothMessage);
 
     Serial.print("Captured Message Length: ");
-    
+
     Serial.println(blueToothMessage.length());
 
     Serial.println(" ");
 
     Serial.println(" ");
-    
+
     blueToothMessage = "";
   }
 

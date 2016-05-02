b24aace73a4b1167a9657e1bd6a78f0a7cded69d
shardy <hardy613@gmail.com>
2016-05-01 23:33:26 -0400

    removed empty space

--- arcc.ino
+++ arcc.ino
@@ -1,15 +1,14 @@
 #include <SoftwareSerial.h>
 /**
- *  @file:  basic set up for blueTooth connection with my phone
- *          I would like to add a WiiMote in the future 
- *          right now we are just reading the data we send it, 
- *          just need a steering wheel (hence wiimote).
- *          
- *  @uses:  HC-06 BlueTooth Module
- *          Adriod phone (Nexus 5x) 
- *         
- */
+    @file:  basic set up for blueTooth connection with my phone
+            I would like to add a WiiMote in the future
+            right now we are just reading the data we send it,
+            just need a steering wheel (hence wiimote).
 
+    @uses:  HC-06 BlueTooth Module
+            Andriod phone (Nexus 5x)
+
+*/
 #define rxPin 4
 
 #define txPin 2
@@ -26,9 +25,9 @@ String blueToothMessage;
 SoftwareSerial blueTooth(rxPin, txPin);
 
 /**
- * Set our serial and bluetooth buad rates
- * Set the RX and TX pins for bluetooth comms
- */
+   Set our serial and bluetooth buad rates
+   Set the RX and TX pins for bluetooth comms
+*/
 void setup()
 {
   Serial.begin(baudRate);
@@ -48,13 +47,21 @@ void loop()
 
   blueTooth.flush();
 
+  Serial.print("blueTooth connedted: ");
+
+  Serial.println(blueTooth.connected());
+
   if (!blueTooth.available())
-  {//end if there is nothing to read
+  { //end if there is nothing to read
+    Serial.print("blueTooth connedted: ");
+
+    Serial.println(blueTooth.connected());
+
     return;
   }
 
   while (blueTooth.available())
-  {//we read the char and add it to the string
+  { //we read the char and add it to the string
     blueToothVal = blueTooth.read();
 
     blueToothMessage += blueToothVal;
@@ -67,7 +74,7 @@ void loop()
   }
 
   if (blueToothMessage.length())
-  {//output captured data to the serial montior
+  { //output captured data to the serial montior
     Serial.print("Captured Message: ");
 
     Serial.println(blueToothMessage);
@@ -85,7 +92,7 @@ void loop()
 
   //we can get 2 char from our connection with this
   //if we remove it we can sometimes only get one char
-  delay(5);
+  delay(100);
 }
 
 

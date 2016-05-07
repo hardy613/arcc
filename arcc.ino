311f1c170c61467e6ca270cdb97eb2c8c43577da
shardy <hardy613@gmail.com>
2016-05-07 17:13:37 -0400

    Startign steering tests

--- arcc.ino
+++ arcc.ino
@@ -9,90 +9,108 @@
             Andriod phone (Nexus 5x)
 
 */
-#define rxPin 4
+//bluetooth connection
+#define rxPin 4           //bluetooth tx pin
+#define txPin 2           //bluetooth rx pin
+#define bluetoothStatePin 7  //bluetooth state pin
+#define baudRate 115200   //AT+BUAD8 ammount of data we can pass
 
-#define txPin 2
+//L293D Connection
+#define steeringMotor1 10 //L293D pin 10
+#define steeringMotor2 9  //L293D pin 14
+#define driveMotor1 5     //L293D pin 2
+#define driveMotor2 6     //L293D pin 7
 
-//AT+BUAD8
-#define baudRate 115200
-
-char blueToothVal;
-
-String blueToothMessage;
+//bluetooth data
+String blueToothVal;
 
 //setting this up allows us to send updates to the
 //board without having to remove wires.
 SoftwareSerial blueTooth(rxPin, txPin);
 
 /**
-   Set our serial and bluetooth buad rates
-   Set the RX and TX pins for bluetooth comms
+ * Begin our serial and bluetooth buad rates
+ * Set the State, RX and TX pins for bluetooth comms
+ * Set pins for steering and driving motors
 */
 void setup()
 {
-  Serial.begin(baudRate);
-
-  blueTooth.begin(baudRate);
-
+  //bluetooth pins
+  pinMode(bluetoothStatePin, INPUT);
   pinMode(rxPin, INPUT);
-
   pinMode(txPin, OUTPUT);
-
+  //L293D Pins
+  pinMode(steeringMotor1, OUTPUT);
+  pinMode(steeringMotor2, OUTPUT);
+  pinMode(driveMotor1, OUTPUT);
+  pinMode(driveMotor2, OUTPUT);
+  //open communication
+  Serial.begin(baudRate);
+  blueTooth.begin(baudRate);
+  
   Serial.println("Starting: ...");
 }
 
 void loop()
 {
   Serial.flush();
-
   blueTooth.flush();
 
-  Serial.print("blueTooth connedted: ");
-
-  Serial.println(blueTooth.connected());
-
-  if (!blueTooth.available())
-  { //end if there is nothing to read
-    Serial.print("blueTooth connedted: ");
-
-    Serial.println(blueTooth.connected());
-
-    return;
+  if (digitalRead(bluetoothStatePin) == LOW)
+  {//bluetooth connection lost
+    Serial.print("Connection Lost: ");
+    Serial.println(digitalRead(bluetoothStatePin));
+    allStopARCC();
   }
-
-  while (blueTooth.available())
-  { //we read the char and add it to the string
-    blueToothVal = blueTooth.read();
-
-    blueToothMessage += blueToothVal;
-
-    Serial.print("Captured Char: ");
-
-    Serial.println(blueToothVal);
-
-    blueToothVal = NULL;
-  }
-
-  if (blueToothMessage.length())
-  { //output captured data to the serial montior
-    Serial.print("Captured Message: ");
-
-    Serial.println(blueToothMessage);
-
-    Serial.print("Captured Message Length: ");
-
-    Serial.println(blueToothMessage.length());
-
-    Serial.println(" ");
-
-    Serial.println(" ");
-
-    blueToothMessage = "";
+  else
+  {//we're connected
+    Serial.print("Connected: ");
+    Serial.println(digitalRead(bluetoothStatePin));
+    
+    if (blueTooth.available())
+    { //we read the char and add it to the string
+      blueToothVal = blueTooth.read();
+  
+      Serial.print("Captured Char: ");
+      Serial.println(blueToothVal);
+    }
+  
+    if (blueToothVal == "L") 
+    {//left
+        analogWrite(steeringMotor1, 200);  
+        analogWrite(steeringMotor2, 0); 
+        analogWrite(driveMotor1, 0);  
+        analogWrite(driveMotor2, 0);
+    } 
+    else if (blueToothVal == "R") 
+    {//right
+        analogWrite(steeringMotor1, 0);  
+        analogWrite(steeringMotor2, 200); 
+        analogWrite(driveMotor1, 0);  
+        analogWrite(driveMotor2, 0);  
+    }
+    else 
+    {
+      allStopARCC();
+    }
   }
 
   //we can get 2 char from our connection with this
   //if we remove it we can sometimes only get one char
-  delay(100);
+  delay(50);
+}
+
+/**
+ * Let's be careful and try to keep the car safe
+ * Turns off all motors
+ */
+void allStopARCC() 
+{
+  //make it so number 1
+  analogWrite(steeringMotor1, 0);  
+  analogWrite(steeringMotor2, 0); 
+  analogWrite(driveMotor1, 0);  
+  analogWrite(driveMotor2, 0);
 }
 
 

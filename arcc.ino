54521a8bb16da1c79da2234cedc13dde10b8beb5
shardy <hardy613@gmail.com>
2016-05-08 12:10:43 -0400

    with 5V - drive motor is going, steering might need more power (12V)

--- arcc.ino
+++ arcc.ino
@@ -32,7 +32,7 @@ SoftwareSerial blueTooth(rxPin, txPin);
  * Begin our serial and bluetooth buad rates
  * Set the State, RX and TX pins for bluetooth comms
  * Set pins for steering and driving motors
-*/
+ */
 void setup()
 {
   //bluetooth pins
@@ -54,21 +54,18 @@ void setup()
 void loop()
 {
   Serial.flush();
-
-  if (digitalRead(bluetoothStatePin) == LOW)
-  {//bluetooth connection lost
-    Serial.print("Connection Lost: ");
-    Serial.println(digitalRead(bluetoothStatePin));
-    allStopARCC();
-  }
-  else
-  {//we're connected
-    Serial.print("Connected: ");
-    Serial.println(digitalRead(bluetoothStatePin));
-    
+//Uncomment below once we've connected the bluetooth state pin
+//  if (digitalRead(bluetoothStatePin) == LOW)
+//  {//bluetooth connection lost
+//    Serial.print("Connection Lost: ");
+//    Serial.println(digitalRead(bluetoothStatePin));
+//    allStopARCC();
+//  }
+//  else
+//  {//we're connected    
     if (blueTooth.available())
-    { //we read the char and add it to the string
-      blueToothVal = blueTooth.read();
+    {
+      blueToothVal = char(blueTooth.read());
   
       Serial.print("Captured Char: ");
       Serial.println(blueToothVal);
@@ -91,12 +88,25 @@ void loop()
         analogWrite(driveMotor1, 0);  
         analogWrite(driveMotor2, 0);  
     }
+    else if (blueToothVal == "F") 
+    {//forward
+        analogWrite(steeringMotor1, 0);  
+        analogWrite(steeringMotor2, 0); 
+        analogWrite(driveMotor1, 200);  
+        analogWrite(driveMotor2, 0);  
+    }
+    else if (blueToothVal == "B") 
+    {//forward
+        analogWrite(steeringMotor1, 0);  
+        analogWrite(steeringMotor2, 0); 
+        analogWrite(driveMotor1, 0);  
+        analogWrite(driveMotor2, 200);  
+    }
     else 
     {
       allStopARCC();
     }
-  }
-
+  //}
   //we can get 2 char from our connection with this
   //if we remove it we can sometimes only get one char
   delay(50);

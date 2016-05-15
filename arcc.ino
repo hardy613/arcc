7d5ce5dc86a96dd67502874d650e2f99306d2c11
shardy <shardy@bsl.com>
2016-05-15 15:20:49 -0400

    moving to lib folder

--- arcc.ino
+++ arcc.ino
@@ -1,6 +1,6 @@
 #include <SoftwareSerial.h>
 //my libraries
-#include "Arcc.h"
+#include "libraries/Arcc/Arcc.h"
 /**
     @file:  basic set up for blueTooth connection with my phone
             I would like to add a WiiMote in the future

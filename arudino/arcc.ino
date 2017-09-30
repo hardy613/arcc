5b171e509cc5e2cccb1723e5c850b423f9b03098
Scott Hardy <hardy613@gmail.com>
2017-09-29 21:33:42 -0400

    removed PING sensor

--- arudino/arcc.ino
+++ arudino/arcc.ino
@@ -8,7 +8,6 @@
 	just need a steering wheel (hence wiimote).
 
 	@uses:  HC-06 BlueTooth Module - communication
-	HC-SR04 Sensor - distance sensor
 	Andriod phone (Nexus 5x) - steering wheel
 
  */
@@ -22,13 +21,9 @@ const int steeringMotorLeft   = 10;
 const int steeringMotorRight  = 9;  
 const int driveMotorForward   = 5;     
 const int driveMotorBackward  = 6;     
-//HC-SR04
-const int echoPin = 8;
-const int trigPin = 7;
+
 //bluetooth data
 String blueToothVal;
-//HC-SR04 safe distance, try to save the car from crashing 
-int safeDisitanceInches = 4;
 //setting this up allows us to send updates to the
 //board without having to remove wires.
 SoftwareSerial BlueTooth(rxPin, txPin);
@@ -46,10 +41,6 @@ void setup()
 	pinMode(rxPin, INPUT);
 	pinMode(txPin, OUTPUT);
 
-	//HC-SR04
-	pinMode(trigPin, OUTPUT);
-	pinMode(echoPin, INPUT);
-
 	//open communication
 	Serial.begin(baudRate);
 	BlueTooth.begin(baudRate);
@@ -59,21 +50,6 @@ void setup()
 
 void loop()
 {  
-	// establish variables for duration of the ping, 
-	// and the distance result in inches and centimeters:
-	long duration, inches, cm;
-	// Read the signal from the sensor: a HIGH pulse whose
-	// duration is the time (in microseconds) from the sending
-	// of the ping to the reception of its echo off of an object
-	// and convert the time into a distance.
-	digitalWrite(trigPin, LOW);
-	delayMicroseconds(2);
-	digitalWrite(trigPin, HIGH);
-	delayMicroseconds(10);
-	digitalWrite(trigPin, LOW);
-	duration = pulseIn(echoPin, HIGH);
-	inches = microsecondsToInches(duration);
-	cm = microsecondsToCentimeters(duration);
 	// we're connected   
 	if (BlueTooth.available())
 	{
@@ -82,67 +58,17 @@ void loop()
 		Serial.println(blueToothVal);      
 	}
 
-	if (blueToothVal == "l") 
-	{// left
+	if (blueToothVal == "l")  {// left
 		Arcc.left(200);
-	} 
-	else if (blueToothVal == "r") 
-	{// right
+	} else if (blueToothVal == "r") {// right
 		Arcc.right(200); 
-	}
-	else if (blueToothVal == "f") 
-	{// forward
-		if(inches < safeDisitanceInches) 
-		{// NO CHRASHING!
-			blueToothVal = "s";
-			BlueTooth.print(inches);
-			BlueTooth.print("in, ");
-			BlueTooth.print(cm);
-			BlueTooth.print("cm");
-			BlueTooth.println();
-			Arcc.allStop();
-		} else {
+	} else if (blueToothVal == "f") {// forward
 			Arcc.forward(100); 
-		}
-	}
-	else if (blueToothVal == "b") 
-	{// backward
+	} else if (blueToothVal == "b") {// backward
 		Arcc.backward(200); 
-	}
-	else if (blueToothVal == "v") 
-	{// straight
+	} else if (blueToothVal == "v") {// straight
 		Arcc.straight(); 
-	}
-	else 
-	{
+	} else {
 		Arcc.allStop();
 	}
-
-	//  }
 }
-/**
- * THX: https://gist.github.com/flakas
- * 
- * According to Parallax's datasheet for the PING))), there are
- * 73.746 microseconds per inch (i.e. sound travels at 1130 feet per
- * second).  This gives the distance travelled by the ping, outbound
- * and return, so we divide by 2 to get the distance of the obstacle.
- * See: http://www.parallax.com/dl/docs/prod/acc/28015-PING-v1.3.pdf
- */
-long microsecondsToInches(long microseconds)
-{
-	return microseconds / 74 / 2;
-}
-/**
- * THX: https://gist.github.com/flakas
- * 
- * The speed of sound is 340 m/s or 29 microseconds per centimeter.
- * The ping travels out and back, so to find the distance of the
- * object we take half of the distance travelled.
- */
-long microsecondsToCentimeters(long microseconds)
-{
-	return microseconds / 29 / 2;
-}
-
-
